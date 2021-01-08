2d57d3d5e4d48bc21bbf482f8f7fe9101b2395cb
Leon Feng <leon@feng.am>
2021-01-09 03:45:55 +0400

    Replace secrets.js with environment variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,18 @@
-const { accessToken, spaceId } = require("./secrets");
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 
 module.exports = {
   siteMetadata: {
     title: "AudioC0RE",
+    siteUrl: "https://audioc0re-main.netlify.app/",
   },
   plugins: [
     {
       resolve: "gatsby-source-contentful",
       options: {
-        accessToken: accessToken,
-        spaceId: spaceId,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
       },
     },
     "gatsby-plugin-sharp",
