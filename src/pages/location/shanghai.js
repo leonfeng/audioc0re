66e4e15ea30a244d965cdc36bd09a675762b177c
Leon Feng <leon@feng.am>
2021-02-19 07:51:18 +0000

    feat(locations): add hero images

--- src/pages/location/shanghai.js
+++ src/pages/location/shanghai.js
@@ -1,7 +1,19 @@
 import * as React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 
 const ShanghaiPage = () => {
-  return <h1>Shanghai</h1>;
+  return (
+    <>
+      <h1>Shanghai</h1>
+      <StaticImage
+        src="../../images/alejo-meza-H23f-2byY_E-unsplash.jpg"
+        alt="Shanghai skyline at night"
+        placeholder="blurred"
+        width={1184}
+        height={652}
+      />
+    </>
+  );
 };
 
 export default ShanghaiPage;
